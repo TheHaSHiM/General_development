196f7694c538b27c461069aa7dfe1f91b15f9ff1
TheHaSHiM <226206372+TheHaSHiM@users.noreply.github.com>
2025-12-08 19:06:03 +0200

    lab08

---
+++ Labs/lab08/contract.js
@@ -0,0 +1,29 @@
+function contract(fn, ...types) {
+    return function(...args) {
+        for (let i = 0; i < types.length-1; i++) {
+            if (typeof args[i] === types[i].name.toLowerCase()) {
+                continue;
+            } else {
+                throw new TypeError('Error');
+            }
+        }
+        const result = fn(...args);
+        if (typeof result === types[types.length-1].name.toLowerCase()) {
+            return result;
+        }
+        else {
+            throw new TypeError('Error');
+        }
+    };
+}  
+
+
+const add = (a, b) => a + b;
+const addNumbers = contract(add, Number, Number, Number);
+const res1 = addNumbers(2, 3);
+console.dir(res1); 
+
+const concat = (s1, s2) => s1 + s2;
+const concatStrings = contract(concat, String, String, String);
+const res2 = concatStrings('Hello ', 'world!');
+console.dir(res2); 
